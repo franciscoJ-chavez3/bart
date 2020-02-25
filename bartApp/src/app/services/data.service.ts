2dd2786fe8d94bc6b0aaf1e337df659a8642150a
Code <code@COCTL-41295.local>
2020-02-25 15:28:31 -0800

    stuff

--- bartApp/src/app/services/data.service.ts
+++ bartApp/src/app/services/data.service.ts
@@ -7,6 +7,7 @@ import { IStation } from '../interfaces/i-station';
 })
 
 export class DataService {
+  
   private stnInfoPt1 = 'http://api.bart.gov/api/stn.aspx?cmd=stninfo&orig='; 
   private stnInfoPt2 = '';  //put station abbreviations here
   private stnInfoPt3 = '&key=Z5RS-PL6X-97JT-DWE9&json=y'; //put your key here
